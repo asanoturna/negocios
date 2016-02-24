7a2c6a1608f6333f7342032a0d646c5b45802e4b
Rockman <gustavoandrade.net@gmail.com>
2016-02-24 16:55:55 -0300

    error form

--- views/visits/_form.php
+++ views/visits/_form.php
@@ -15,15 +15,16 @@ use yii\widgets\MaskedInput;
 <div class="visits-form">
 
 <?php
-if(preg_match('/(?i)msie [5-8]/',$_SERVER['HTTP_USER_AGENT']))
-{
-    // if IE<=8
-    // include ( TEMPLATEPATH . '/noie.php' );
-    // exit;
-    echo "<div class=\"alert alert-danger\" role=\"alert\"><span class=\"glyphicon glyphicon-exclamation-sign\" aria-hidden="true"></span> Você está usando o navegador Internet Explorer 8 (ou inferior) ! </br>Para melhor visualização e funcionamento do sistema, utilize o navegador Mozilla Firefox ou Google Chrome</div>";
-}else{
-    // if IE>8
-}
+// if(preg_match('/(?i)msie [5-8]/',$_SERVER['HTTP_USER_AGENT']))
+// {
+//     // if IE<=8
+//     // include ( TEMPLATEPATH . '/noie.php' );
+//     // exit;
+//     echo "<div class=\"alert alert-danger\" role=\"alert\"><span class=\"glyphicon glyphicon-exclamation-sign\" aria-hidden="true"></span> Você está usando o navegador Internet Explorer 8 (ou inferior) ! </br>Para melhor visualização e funcionamento do sistema, utilize o navegador Mozilla Firefox ou Google Chrome</div>";
+// }
+// // else{
+// //     // if IE>8
+// // }
 ?>
 
     <?php $form = ActiveForm::begin([
