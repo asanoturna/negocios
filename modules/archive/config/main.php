96c3094b52f34b5e2f404df1dcb68ee76cc0e93a
Gustavo G. Andrade <gugoan@hotmail.com>
2016-12-18 23:35:48 -0200

    fic composer

--- modules/archive/config/main.php
+++ modules/archive/config/main.php
@@ -2,7 +2,7 @@
 
 return [
 	'params' => [
-		'visitModuleName' => 'Visitas dos Gerentes',
+		'visitModuleName' => 'Arquivos',
 		'visitAttachment' => '../../Anexos_visitas_anexos/',
 		'visitImages'     => '../../Anexos_visitas_anexos/',
 	]
